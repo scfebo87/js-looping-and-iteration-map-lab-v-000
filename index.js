7e8b53d1c8cdc7d51f0799817d63c5c1fbaf57d5
Sean Christopher Febo <scf@techie.com>
2018-07-16 22:42:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+function lowerCaseDrivers(array) { 
+  return array.map(driver => driver.toLowerCase());
+}
+
 function attributesToPhrase(array) { 
   return arrary.map(driver => `${driver.name} is from ${driver.hometown}`);
 }
